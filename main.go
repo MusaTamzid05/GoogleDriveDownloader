052597d1a2d2a12a8dd65d03196458cb26fc7056
MusaTamzid05 <tamzid101@gmail.com>
2020-11-15 16:23:20 +0600

    flag added.

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "google_drive_downloader/downloader"
+import (
+	"flag"
+	"google_drive_downloader/downloader"
+	"log"
+)
+
+func Usage() {
+	log.Fatalln("-id id -dst dstPath")
+}
 
 func main() {
 
-	downloader := downloader.NewDownloader("1AjXlwT19u1WlxjfEimtIaILIKCrfeIAV")
-	downloader.Start("data.txt")
+	idPtr := flag.String("id", "", "google drive id")
+	dstPtr := flag.String("dst", "", "path to download data")
+	flag.Parse()
+
+	if *idPtr == "" || *dstPtr == "" {
+		Usage()
+	}
+
+	downloader := downloader.NewDownloader(*idPtr)
+	downloader.Start(*dstPtr)
 }
